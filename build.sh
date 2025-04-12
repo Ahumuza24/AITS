478199e6ad0a65c44fa10d619b782d2ac6d12352
Cedric Ahumuza <ahumuzacedric@gmail.com>
2025-04-12 20:46:22 +0300

    changes

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-gunicorn -b 0.0.0.0:8000 --chdir /app/src/app mysite.wsgi:application
+gunicorn -b 0.0.0.0:8000 --chdir /backend
