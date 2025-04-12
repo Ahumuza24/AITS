65d38e08d281acdf2c13228d2d03a976990ee588
Cedric Ahumuza <ahumuzacedric@gmail.com>
2025-04-12 20:59:16 +0300

    changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-yoursecretkeyhere'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
